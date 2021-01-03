710d8b47da25a11f05868fd1f1160f6e604f8f5c
Bas Bossink <bas.bossink@gmail.com>
2021-01-03 21:49:09 +0100

    Finish kata

--- vbowling.v
+++ vbowling.v
@@ -3,11 +3,21 @@ module main
 fn score(rolls []int) int {
 	number_of_pins := 10
 	mut score := 0
-	for i := 0; i < rolls.len - 1; i += 2 {
-		if rolls[i] + rolls[i+1] == number_of_pins {
-			score += rolls[i+2]
-		} 
-		score += rolls[i] + rolls[i+1]
+	for i := 0; i < rolls.len - 1; {
+		if rolls[i] == number_of_pins {
+			score += number_of_pins
+			score += rolls[i + 1] + rolls[i + 2]
+			if i == rolls.len - 3 {
+				return score
+			}
+			i++
+			continue
+		}
+		if rolls[i] + rolls[i + 1] == number_of_pins {
+			score += rolls[i + 2]
+		}
+		score += rolls[i] + rolls[i + 1]
+		i += 2
 	}
 	return score
 }
--- vbowling_test.v
+++ vbowling_test.v
@@ -10,6 +10,28 @@ fn test_ones_game() {
 
 fn test_spare_should_count_next_as_bonus() {
 	mut rolls := [5, 5]
-	rolls  << []int{len: 18, init: 1}
+	rolls << []int{len: 18, init: 1}
 	assert score(rolls) == 29
+}
+
+fn test_spare_should_count_next_as_bonus_in_last_frame() {
+	mut rolls := []int{len: 19, init: 1}
+	rolls << [9, 1]
+	assert score(rolls) == 29
+}
+
+fn test_strike_should_count_next_as_bonus() {
+	mut rolls := [10]
+	rolls << []int{len: 18, init: 1}
+	assert score(rolls) == 30
+}
+
+fn test_strike_should_count_next_as_bonus_in_last_frame() {
+	mut rolls := []int{len: 18, init: 1}
+	rolls << [10, 1, 1]
+	assert score(rolls) == 30
+}
+
+fn test_perfect_game_should_score_300() {
+	assert score([]int{len: 12, init: 10}) == 300
 }
